178996b6d37f74ed4c1b1b4bc58499f8104e7624
ashutoshsao <ashutoshsao17@gmail.com>
2026-08-12 19:09:54 +0530

    Fix broken Apollo GraphQL scaffold, simplify User schema to id/username/password

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,35 +1,65 @@
 import fs from "node:fs/promises";
-import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
 import * as path from "node:path";
-import { buildSchema } from "graphql";
-const schemaString = await fs.readFile(path.join(import.meta.dir, "./schema.gql"), "utf-8");
-const schema = buildSchema(schemaString)
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
 
-const authMiddleware = (req, res, next) => {
-  req.authHeader = req.headers.authorization;
-  next();
-}
+const schemaString = await fs.readFile(
+  path.join(import.meta.dir, "./schema.gql"),
+  "utf-8",
+);
+
+type User = {
+  id: string;
+  username: string;
+  password: string;
+};
+
+const users: User[] = [];
+let nextId = 1;
 
 const root = {
   Query: {
-    getUser: ({ id: string }, req) => {
-      //get user and return
-      console.log(id)
-    }
+    getUser: (_parent: unknown, args: { id: string }) => {
+      return users.find((user) => user.id === args.id) ?? null;
+    },
+    getUsers: () => users,
   },
   Mutation: {
-    createUser(input: CreateUserInput): callback
-  }
-}
+    createUser: (
+      _parent: unknown,
+      args: { input: { username: string; password: string } },
+    ) => {
+      const user: User = { id: String(nextId++), ...args.input };
+      users.push(user);
+      return user;
+    },
+    updateUser: (
+      _parent: unknown,
+      args: { input: { id: string; username?: string; password?: string } },
+    ) => {
+      const user = users.find((u) => u.id === args.input.id);
+      if (!user) {
+        throw new Error(`No user with id ${args.input.id}`);
+      }
+      Object.assign(user, args.input);
+      return user;
+    },
+    deleteUser: (_parent: unknown, args: { id: string }) => {
+      const index = users.findIndex((user) => user.id === args.id);
+      if (index === -1) return false;
+      users.splice(index, 1);
+      return true;
+    },
+  },
+};
 
 const server = new ApolloServer({
-  typeDefs: schema,
+  typeDefs: schemaString,
   resolvers: root,
-})
+});
 
 const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 }
-})
+  listen: { port: 4000 },
+});
 
-console.log(`server started at ${url}`)
+console.log(`server started at ${url}`);
